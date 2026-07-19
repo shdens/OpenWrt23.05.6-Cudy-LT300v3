17bf7775db282cc7d382708e92208c04e58aa5c3
shdens <80463987+shdens@users.noreply.github.com>
2026-07-19 15:21:26 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-# Принудительно задаем целевую платформу MediaTek MT76x8
-echo 'CONFIG_TARGET_ramips=y' >> .config
+
+# 1. Исправляем опечатку "Gang" в патче DTS, если она там осталась
+sed -i 's/Gang_ACTIVE_LOW/GPIO_ACTIVE_LOW/g' target/linux/ramips/dts/mt7628an_cudy_lt300-v3.dts
+
+# 2. Модифицируем файл сборки image/mt76x8.mk
+# Мы временно очищаем список TARGET_DEVICES, чтобы система вообще не знала про Alfa Network
+sed -i 's/TARGET_DEVICES +=/# TARGET_DEVICES +=/g' target/linux/ramips/image/mt76x8.mk
+
+# 3. Принудительно возвращаем в список сборки ТОЛЬКО ваш Cudy
+echo "TARGET_DEVICES += cudy_lt300-v3" >> target/linux/ramips/image/mt76x8.mk
+
+# 4. Прописываем конфигурацию в .config
+echo 'CONFIG_TARGET_ramips=y' > .config
 echo 'CONFIG_TARGET_ramips_mt76x8=y' >> .config
-
-# Жестко прописываем сборку только для вашего Cudy LT300 v3 из патча
 echo 'CONFIG_TARGET_ramips_mt76x8_DEVICE_cudy_lt300-v3=y' >> .config
-
-# Отключаем генерацию образов для всех остальных устройств платформы
 echo 'CONFIG_TARGET_MULTI_PROFILE=n' >> .config
